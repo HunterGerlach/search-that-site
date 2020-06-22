eb6ca9e54ea0d094d470abc6314f08d8e243960c
Hunter Gerlach <hgerlach@redhat.com>
2020-06-22 11:18:00 -0400

    Add Kubernetes site option

--- options-site.js
+++ options-site.js
@@ -1,9 +1,10 @@
 let sitePage = document.getElementById('siteDiv');
   const google = {"url": "google.com", "color": {"primary": "#3aa757", "secondary": "#000"}};
   const redhat = {"url": "access.redhat.com", "color": {"primary": "#EE0000", "secondary": "#000"}};
+  const k8s    = {"url": "kubernetes.io", "color": }"primary": "#326de6", "secondaty": "#fff"}};
   const so     = {"url": "stackoverflow.com", "color": {"primary": "#f9bb2d", "secondary": "#000"}};
   const ibm    = {"url": "cloud.ibm.com", "color": {"primary": "#0530ad", "secondary": "#fff"}};
-  const kSites = [google, redhat, so, ibm];
+  const kSites = [google, redhat, k8s, so, ibm];
   function constructSiteOptions(kSites) {
     for (let item of kSites) {
       let site = document.createElement('p');
