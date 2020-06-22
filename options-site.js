7039a99eb09bca9f372cb9b202ffeb004df7e0f5
Hunter Gerlach <hgerlach@redhat.com>
2020-06-22 11:19:57 -0400

    --amend

--- options-site.js
+++ options-site.js
@@ -1,7 +1,7 @@
 let sitePage = document.getElementById('siteDiv');
   const google = {"url": "google.com", "color": {"primary": "#3aa757", "secondary": "#000"}};
   const redhat = {"url": "access.redhat.com", "color": {"primary": "#EE0000", "secondary": "#000"}};
-  const k8s    = {"url": "kubernetes.io", "color": {"primary": "#326de6", "secondaty": "#fff"}};
+  const k8s    = {"url": "kubernetes.io", "color": {"primary": "#326de6", "secondary": "#fff"}};
   const so     = {"url": "stackoverflow.com", "color": {"primary": "#f9bb2d", "secondary": "#000"}};
   const ibm    = {"url": "cloud.ibm.com", "color": {"primary": "#0530ad", "secondary": "#fff"}};
   const kSites = [google, redhat, k8s, so, ibm];
